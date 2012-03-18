df73035e0ac1dcb553e34ee8f1db804a5f18cf9c
SUZUKI Masashi <m15.suzuki.masashi@gmail.com>
2012-03-18 22:36:28 +0900

    生成スクリプトを書いてみる

---
+++ create_html.sh
@@ -0,0 +1,11 @@
+#! /usr/bin/zsh -eu
+OUTPUTDIR=`pwd`/build
+
+mkdir -p ${OUTPUTDIR}
+for slide in $( ls )
+do
+    if [ -d ${slide} ] && [ ${slide} != 'build' ]
+    then
+        rst2s5 `pwd`/${slide}/source/index.rst ${OUTPUTDIR}/${slide}.html
+    fi
+done
