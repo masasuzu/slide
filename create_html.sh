374e12192be3dc9cdd0eedc6d6b3ae1f59018f4d
SUZUKI Masashi <m15.suzuki.masashi@gmail.com>
2012-03-18 23:30:11 +0900

    テーマを変えてみる

--- create_html.sh
+++ create_html.sh
@@ -6,6 +6,6 @@ for slide in $( ls )
 do
     if [ -d ${slide} ] && [ ${slide} != 'build' ]
     then
-        rst2s5 `pwd`/${slide}/source/index.rst ${OUTPUTDIR}/${slide}.html
+        rst2s5 --theme=small-black `pwd`/${slide}/source/index.rst ${OUTPUTDIR}/${slide}.html
     fi
 done
